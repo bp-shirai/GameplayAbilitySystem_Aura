15647fd310ba6992b6a1c57d3f4181d29d39caa7
bp.shirai <bp.shirai@gmail.com>
2024-01-12 16:39:55 +0900

    Enemy character

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,12 +4,20 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,4 +62,60 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (CursorHit.bBlockingHit)
+	{
+		LastActor = ThisActor;
+		ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+		// Line trace from cursor. There are several scenarios:
+		// A. LastActor is null && ThisActor is null
+		//		- Do nothing.
+		// B. LastActor is null && ThisActor is valid
+		//		- Highlight ThisActor
+		// C. LastActor is valid && ThisActor is null
+		//		- UnHighlight LastActor
+		// D. Both actors are valid, but LastActor != ThisActor
+		//		- UnHighlight LastActor, and Highlight ThisActor
+		// E. Both actors are valid, and are the same actor
+		//		- Do nothing
+		if (LastActor == nullptr)
+		{
+			if (ThisActor != nullptr)
+			{
+				// Case B
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case A - do nothing
+			}
+		}
+		else
+		{
+			if (ThisActor == nullptr)
+			{
+				// Case C
+				LastActor->UnHighlightActor();
+			}
+			else
+			{
+				if (LastActor != ThisActor)
+				{
+					// Case D
+					LastActor->UnHighlightActor();
+					ThisActor->HighlightActor();
+				}
+				else
+				{
+					// Case E - do nothing
+				}
+			}
+		}
+	}
 }
